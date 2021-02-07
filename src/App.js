c55ecfaff3058096d2b146967ecbf9d487bcb534
Shankar-Kb <shankar.kb95@gmail.com>
2021-02-07 14:15:28 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ export const App = () => {
     const addTicket = () => {
         let data = {
         title: document.getElementById("title").value,
-        author: document.getElementById("author").value
+        author: document.getElementById("author").value,
+        editMode: false
         }
         
         fetch("http://localhost:3001/posts", {
@@ -17,6 +18,7 @@ export const App = () => {
               })
               .then( resp => resp.json() )
               .then( data => setPosts(posts.concat([data])));
+        document.getElementById("addTicketForm").reset();
     }
 
     function getTickets(){
@@ -32,15 +34,53 @@ export const App = () => {
         fetch(`http://localhost:3001/posts/${id}`,
            { method: "DELETE" })
            .then( resp => resp.json() )
-           .then( data => { console.log(data);
-            setPosts( prevPosts => prevPosts.filter( elem => elem.id !== id));
+           .then( data => { //console.log(data);
+            setPosts( prevPosts => prevPosts.filter( elem => elem.id !== id ));
            });
            console.log("Delete Function Has Executed");
     }
 
-    const editTicket = (id) => {
-        console.log("Ticket Has Been Edited");
+    const editTicket = (index) => {
+        setPosts(posts.map( (ele, idx) => {
+            if(idx === index) ele.editMode = true;
+            return ele;
+         }));
     }
+
+    const cancelEdit = (index) => {
+        setPosts(posts.map( (ele, idx) => {
+           if(idx === index) ele.editMode = false;
+           return ele;
+        }));
+    }
+
+    const confirmEdit = (index, id) => {
+        let newTitleValue = document.getElementById("inputEditTitle").value;
+        let newAuthorValue = document.getElementById("inputEditAuthor").value;
+        let newData = {
+            title: newTitleValue,
+            author: newAuthorValue,
+            id: id,
+            editMode: false
+        }
+        fetch(`http://localhost:3001/posts/${id}`,
+           { method: "PUT",
+             body: JSON.stringify(newData),
+             headers: {"Content-Type": "application/json; charset=UTF-8"}
+           })
+           .then( resp => resp.json() )
+           .then( data => { console.log("Ticket Has Been Updated Server Side") })
+
+        setPosts(posts.map( (ele, idx) => {
+           if(idx === index) {
+               ele.title = newTitleValue;
+               ele.author = newAuthorValue;
+               ele.editMode = false;
+           }
+           return ele;
+        }));
+    }
+
 
     React.useEffect(() => {    
         getTickets();
@@ -48,34 +88,41 @@ export const App = () => {
 
     return (
             <div className='posts'>
-                <div className="new-ticket-box">
-                <input id="title" type="text" defaultValue="" />
-                <input id="author" type="text" defaultValue="" />
-                <button className="btn btn-secondary" onClick={addTicket}>Add New Ticket</button>
+                <form id="addTicketForm">
+                <div className="input-group">
+                <span className="input-group-text">Ticket Name</span>
+                <input className="form-control" id="title" type="text" defaultValue="" />
+                <span className="input-group-text">Ticket Author</span>
+                <input className="form-control" id="author" type="text" defaultValue="" />
                 </div>
+                </form>
+                <button className="btn btn-secondary" id="addNewTicket" onClick={addTicket}>Add New Ticket</button>
+                
                 <table className="table">
                 <tbody>
                 <tr className="thead-dark"><th>ID</th><th>Ticket Name</th><th>Ticket Author</th><th>Edit/Delte</th></tr>
                 {posts.map((data, index) => (
                     <tr key={index}>
+                    {data.editMode === false ?
+                    <>
                     <td>{data.id}</td><td>{data.title}</td><td>{data.author}</td>
-                    <td><button data-toggle="modal" data-target="#editModal" onClick={() => {editTicket(data.id)}}><i className="fas fa-edit"></i></button>
-                    <button onClick={() => deleteTicket(data.id)}><i className="fas fa-trash"></i></button></td>
-                   </tr>
+                    <td><button className="btn btn-secondary" onClick={() => {editTicket(index)}}><i className="fas fa-edit"></i></button>
+                    <button className="btn btn-secondary" onClick={() => deleteTicket(data.id)}><i className="fas fa-trash"></i></button></td>
+                    </>
+                    :
+                    <>
+                    <td>{data.id}</td>
+                    <td><input className="form-control" type="text" id="inputEditTitle" defaultValue={data.title} /></td>
+                    <td><input className="form-control" type="text" id="inputEditAuthor" defaultValue={data.author} /></td>
+                    <td>
+					<button className="btn btn-secondary" onClick={ ()=>{cancelEdit(index)} }><i className="fas fa-times"></i></button>
+					<button className="btn btn-secondary" onClick={ ()=>{confirmEdit(index, data.id)} }><i className="fas fa-check"></i></button></td>
+                    </>
+                     }
+                    </tr>
                 ))}
                 </tbody>
                 </table>
-                  <div className="modal fade" id="editModal" tabIndex="-1">
-                      <div class="modal-header">
-                          <input id="editTitle" type="text" defaultValue="" />
-                          <input id="editAuthor" type="text" defaultValue="" />
-                      </div>
-      
-                      <div class="modal-footer">
-                          <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                          <button type="button" className="btn btn-primary">Save changes</button>
-                      </div>
-                  </div>
             </div>
     );
 }
